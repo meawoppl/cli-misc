109b064aa5b52da834be01c586131a678cc697de
meawoppl <meawoppl@gmail.com>
2018-12-25 20:51:26 -0700

    More assertions/test

--- oshpark_altium.py
+++ oshpark_altium.py
@@ -10,7 +10,7 @@ EXPECTED_BOTTOM = (".gbl", ".gbo", ".gbs")
 EXPECTED_TOP = (".gtl", ".gto", ".gts")
 EXPECTED = EXPECTED_TOP + EXPECTED_BOTTOM
 
-OUTLINE = (".gm17")
+OUTLINE_SUFFIX = ".gm17"
 
 def is_xln_file(path: str) -> bool:
     assert os.path.exists(path), "Path does not exist:" + path
@@ -18,7 +18,7 @@ def is_xln_file(path: str) -> bool:
     return contents.startswith("M48")
 
 now = datetime.datetime.now()
-default_name = now.strftime("export-%Y%m%d_%h%m%s.zip")
+default_name = now.strftime("export-%Y_%m_%d_%h_%m_%s.zip")
 
 parser = argparse.ArgumentParser(
     description="A utility to take the default exports from altium to the oshpark expected format")
@@ -27,12 +27,15 @@ parser.add_argument("--output", help="The name of the output file", default=defa
 args = parser.parse_args()
 
 assert os.path.isdir(args.folder), "Input folder does not exist:" + args.folder
+print("Creating export file: " + args.output)
 z = zipfile.ZipFile(args.output, "w", zipfile.ZIP_DEFLATED)
 
 
 def write_and_note(path: str, dest: str):
-    print("\t{} --> Zip({})".format(path, dest))
+    print("    {} --> ZIP({})".format(path, dest))
     z.write(path, dest)
+
+required = list(EXPECTED)
 
 for fname in sorted(os.listdir(args.folder)):
     fpath = os.path.join(args.folder, fname)
@@ -40,14 +43,21 @@ for fname in sorted(os.listdir(args.folder)):
         continue
 
     prefix, extension = os.path.splitext(fname)
-    
-    if extension.lower() in EXPECTED:
+    extension = extension.lower()
+
+    # Top/bottom gerbers
+    if extension in EXPECTED:
         write_and_note(fpath, fname)
     
-    if extension.lower() in OUTLINE:
+    # Board outline
+    if extension == OUTLINE_SUFFIX:
         write_and_note(fpath, prefix + ".GKO")
 
-    if extension.lower() == ".txt" and is_xln_file(fpath):
+    if extension == ".txt" and is_xln_file(fpath):
         write_and_note(fpath, prefix + ".XLN")
+
+archived_extensions = set(os.path.splitext(name)[1].lower() for name in z.namelist())
+required_extensions = set(EXPECTED + (".gko", ".xln"))
+assert archived_extensions == required_extensions, required_extensions - archived_extensions 
 
 z.close()
